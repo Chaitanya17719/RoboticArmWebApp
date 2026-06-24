d8bc0dd5ace62ea40473bf587665a9331ec1bc1b
Sanikommu Chaitanya <chaitanyasanikommu6@gmail.com>
2026-06-24 15:17:12 +0530

    Update Firebase configuration with new credentials

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -10,7 +10,7 @@ import {
 } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB0-XbbOjKufubDSwxrITrRvQICGs451lo",
+  /*apiKey: "AIzaSyB0-XbbOjKufubDSwxrITrRvQICGs451lo",
   authDomain: "roboticarmapp.firebaseapp.com",
   projectId: "roboticarmapp",
   storageBucket: "roboticarmapp.firebasestorage.app",
@@ -22,7 +22,14 @@ const firebaseConfig = {
   storageBucket: "roboticarmapp.firebasestorage.app",
   messagingSenderId: "602390886664",
   appId: "1:602390886664:web:c3c09f5b636435d26d54bd",
-  measurementId: "G-7ZX0YLTE4N",
+  measurementId: "G-7ZX0YLTE4N",*/
+  apiKey: "AIzaSyA2emfHNhH9VZ6NqUeM2_kbwzZHHYrgRFo",
+  authDomain: "robotic-arm-7e0f0.firebaseapp.com",
+  projectId: "robotic-arm-7e0f0",
+  storageBucket: "robotic-arm-7e0f0.firebasestorage.app",
+  messagingSenderId: "1070250375561",
+  appId: "1:1070250375561:web:43eb48253a1e198f874958",
+  measurementId: "G-WM7EGC0ZYN"
 };
 
 const app = initializeApp(firebaseConfig);
